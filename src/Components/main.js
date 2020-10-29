a838be4fd2fdf86c60bf4c7aa02547f03ba91b67
Adam Wong <adamwthedev@gmail.com>
2020-10-28 22:09:35 -0700

    created Images folder

--- src/Components/main.js
+++ src/Components/main.js
@@ -21,11 +21,58 @@ const Main = Styled.div`
         }
     }
 
-    .projects {
-        height: 300px;
-        width: 600px;
+    .skills {
+        position: relative;
+        height: 600px;
+        width: 1456px;
+        margin: auto;
         color: gray;
 
+        .languages, .front-end, .back-end, .additional {
+            position: absolute;
+            height: 300px;
+            width: 400px
+        }
+
+        .skills-heading {
+            margin-left: 24px;
+        }
+
+        .languages {
+            top: 24px;
+            border: 1px solid gray;
+        }
+
+        .front-end {
+            top: 300px;
+            left: 352px;
+            background-color: gray;
+            color: white;
+        }
+
+        .back-end {
+            top: 24px;
+            left: 704px;
+            background-color: #ffcc99;
+            color: white;
+        }
+
+        .additional {
+            top: 300px;
+            left: 1056px;
+            border: 1px solid gray;
+            background-color: white;
+            color: gray;
+        }
+
+    }
+
+    .projects {
+        height: 300px;
+        margin-top: 24px;
+        background-color: gray;
+        text-align: center;
+        color: white;
     }
 
 `;
@@ -39,6 +86,20 @@ const main = () => {
                     <p className="greeting">Hey there.</p>
                     <p>Adam is the name.</p>
                     <p>Web development is the game.</p>
+                </section>
+                <section className="skills">
+                    <div className="languages">
+                        <h2 className="skills-heading">Languages</h2>
+                    </div>
+                    <div className="front-end">
+                        <h2 className="skills-heading">Front End</h2>
+                    </div>
+                    <div className="back-end">
+                        <h2 className="skills-heading">Back End</h2>
+                    </div>
+                    <div className="additional">
+                        <h2 className="skills-heading">Additional Tools</h2>
+                    </div>
                 </section>
                 <div className="projects">
                     <h2>Recent Works</h2>
