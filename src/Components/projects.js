888c49baa948bd6817d2e7819f9cdd70e40c8481
Adam Wong <adamwthedev@gmail.com>
2020-11-02 20:44:52 -0800

    added cover photos to Projects component

--- src/Components/projects.js
+++ src/Components/projects.js
@@ -1,22 +1,48 @@
 import React from "react";
 import Styled from "styled-components";
-import SmashUlt from "../Images/smash-ult-cover-500px.png";
-import Xpense from "../Images/xpense-cover-500px.png";
+import SmashUlt from "../Images/smash-ult-cover-light.png";
+import Xpense from "../Images/xpense-cover-light.png";
 
 const Projects = Styled.div`
     margin-top: 24px;
+    padding-top: 80px;
+    padding-bottom: 100px;
     background-color: gray;
     text-align: center;
     color: white;
 
-    .smash-container {
-        position: relative;
-        height: 250px;
-        width: 500px;
-        border: 2px solid white;
+    .projects-display {
+        display: flex;
+        justify-content: space-around;
 
-        div {
-            position: absolute;
+        .cover-container {
+            position: relative;
+            height: 250px;
+            width: 500px;
+    
+            &:hover .content {
+                cursor: pointer;
+                opacity: 1;
+            }
+    
+            img {
+
+                &:hover {
+                    opacity: 0.5;
+                    transition: 0.5s ease;
+                }
+            }
+    
+            .content {
+                position: absolute;
+                top: 0;
+                height: 154px;
+                width: 500px;
+                padding-top: 96px;
+                text-align: center;
+                opacity: 0;
+                transition: 0.5s ease;
+            }
         }
     }
 `;
@@ -24,18 +50,21 @@ const Projects = Styled.div`
 const projects = () => {
     return (
         <Projects>
-            <div className="smash-container">
-                <img src={SmashUlt} ult="cover" />
-                <div>
-                    <h2>Smash Ultimate Stats Tracker</h2>
+            <h1>Latest Works</h1>
+            <section className="projects-display">   
+                <div className="cover-container">
+                    <img src={SmashUlt} ult="cover" />
+                    <div className="content">
+                        <h2>Smash Ultimate Stats Tracker</h2>
+                    </div>
                 </div>
-            </div>
-            <div className="xpense-container">
-                <img src={Xpense} ult="cover" />
-                <div>
-                    <h2>Xpense</h2>
+                <div className="cover-container">
+                    <img src={Xpense} ult="cover" />
+                    <div className="content">
+                        <h2>Xpense</h2>
+                    </div>
                 </div>
-            </div>
+            </section>
         </Projects>
     );
 };
