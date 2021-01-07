742d4af7d086f04af9b120db29c607e71f55b175
Adam Wong <adamwthedev@gmail.com>
2021-01-06 16:05:00 -0800

    modifying layout of Smashult component

--- src/Components/smashUlt.js
+++ src/Components/smashUlt.js
@@ -40,20 +40,49 @@ const SmashUlt = Styled.div`
         }
     }
     
-    .get-started-list {
-        list-style: none;
-        padding-left: 0px;
+    .get-started {
+        display: grid;
+        grid-template-columns: repeat(2,50%);
 
-        .get-started-list-item {
-            margin-bottom: 16px;
+        .get-started-list {
+            list-style: none;
+            padding-left: 0px;
 
-            .get-started-label {
-                font-weight: bold;
+            .get-started-list-item {
+                margin-bottom: 16px;
+
+                .get-started-label {
+                    font-weight: bold;
+                }
+
+                .get-started-detail {
+                    display: inline;
+                }
             }
+        }
 
-            .get-started-detail {
-                display: inline;
+        .get-started-pic {
+            border: 2px solid black;
+            text-align: center;
+        }
+    }
+
+    .navigation-div {
+        display: grid;
+        grid-template-columns: repeat(2, 50%);
+        margin-bottom: 24px;
+
+        .navigation-list {
+            padding-left: 16px;
+
+            .navigation-list-item {
+                margin-bottom: 24px;
             }
+        }
+
+        .navigation-pic {
+            border: 2px solid black;
+            text-align: center;
         }
     }
 `;
@@ -98,80 +127,100 @@ const smashUlt = () => {
                 </ul>
             </section>
             <section className="get-started">
-                <p>To get started, you may use the following login:</p>
-                <ul className="get-started-list">
-                    <li className="get-started-list-item">
-                        <label className="get-started-label">Username: </label>
-                        <p className="get-started-detail">guest</p>
-                    </li>
-                    <li className="get-started-list-item">
-                        <label className="get-started-label">Password: </label>
-                        <p className="get-started-detail">welcome!</p>
-                    </li>
-                </ul>
+                <div>
+                    <p>To get started, you may use the following login:</p>
+                    <ul className="get-started-list">
+                        <li className="get-started-list-item">
+                            <label className="get-started-label">Username: </label>
+                            <p className="get-started-detail">guest</p>
+                        </li>
+                        <li className="get-started-list-item">
+                            <label className="get-started-label">Password: </label>
+                            <p className="get-started-detail">welcome!</p>
+                        </li>
+                    </ul>
+                </div>
+                <div className="get-started-pic">
+                    PICTURE HERE
+                </div>
             </section>
             <section className="navigation">
                 <p>Here is a list of all the application pages and their functions:</p>
-                <div>
-                    <h2>Landing</h2>
-                    <ul>
-                        <li>
-                            User can see latest posts on /r/smashbros from Reddit. Data is 
-                            fetched from the Reddit API
-                        </li>
-                        <li>
-                            User can see who the top registered players are as well as 
-                            see the top three characters with the most wins
-                        </li>
-                    </ul>
+                <div className="navigation-div">
+                    <div className="navigation-landing">
+                        <h2>Landing</h2>
+                        <ul className="navigation-list">
+                            <li className="navigation-list-item">
+                                User can see latest posts on /r/smashbros from Reddit. Data is 
+                                fetched from the Reddit API
+                            </li>
+                            <li className="navigation-list-item">
+                                User can see who the top registered players are as well as 
+                                see the top three characters with the most wins
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="navigation-pic">
+                        PICTURE HERE
+                    </div>
                 </div>
-                <div>
-                    <h2>Home</h2>
-                    <ul>
-                        <li>
-                            First page that is seen after logging in and can be accessed by 
-                            clicking on the smash ball icon on the nav bar
-                        </li>
-                        <li>
-                            Quick overview of user's overall stats
-                        </li>
-                    </ul>
+                <div className="navigation-div">
+                    <div className="navigation-home">
+                        <h2>Home</h2>
+                        <ul className="navigation-list">
+                            <li className="navigation-list-item">
+                                First page that is seen after logging in and can be accessed by 
+                                clicking on the smash ball icon on the nav bar
+                            </li>
+                            <li className="navigation-list-item">
+                                Quick overview of user's overall stats
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="navigation-pic">
+                        PICTURE HERE
+                    </div>
                 </div>
-                <div>
-                    <h2>Add Stats</h2>
-                    <ul>
-                        <li>
-                            Page where user can add or subtract wins or losses for a specific 
-                            character matchup
-                        </li>
-                        <li>
-                            To add stats, complete the following:
-                            <ul>
-                                <li>
-                                    To select which character you want to change, begin by clicking 
-                                    either the "Player" or "Opponent" avatar. The white border around 
-                                    the avatar will indicate that its been selected
-                                </li>
-                                <li>
-                                    Next, select the character you desire from the "Character Select" 
-                                    section
-                                </li>
-                                <li>
-                                    Lastly to add stats, simply click the "+" or "-" button to add or 
-                                    subtract stats respectively
-                                </li>
-                            </ul>
-                        </li>
-                        <li>
-                            Users can use the "View By" filter to filter character matches by a specific 
-                            time frame
-                            <ul>
-                                <li>
-                                    "Matchup History" is a history of the filtered matches
-                                </li>
-                            </ul>
-                        </li>
-                    </ul>
+                <div className="navigation-div">
+                    <div className="navigation-add-stats">
+                        <h2>Add Stats</h2>
+                        <ul className="navigation-list">
+                            <li className="navigation-list-item">
+                                Page where user can add or subtract wins or losses for a specific 
+                                character matchup
+                            </li>
+                            <li className="navigation-list-item">
+                                To add stats, complete the following:
+                                <ul>
+                                    <li className="navigation-list-item">
+                                        To select which character you want to change, begin by clicking 
+                                        either the "Player" or "Opponent" avatar. The white border around 
+                                        the avatar will indicate that its been selected
+                                    </li>
+                                    <li className="navigation-list-item">
+                                        Next, select the character you desire from the "Character Select" 
+                                        section
+                                    </li>
+                                    <li className="navigation-list-item">
+                                        Lastly to add stats, simply click the "+" or "-" button to add or 
+                                        subtract stats respectively
+                                    </li>
+                                </ul>
+                            </li>
+                            <li className="navigation-list-item">
+                                Users can use the "View By" filter to filter character matches by a specific 
+                                time frame
+                                <ul>
+                                    <li className="navigation-list-item">
+                                        "Matchup History" is a history of the filtered matches
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="navigation-pic">
+                        PICTURE HERE
+                    </div>
                 </div>
             </section>
         </SmashUlt>
