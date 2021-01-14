912c5825beb37af814aed17b4368eb7edc980a52
Adam Wong <adamwthedev@gmail.com>
2021-01-13 18:53:39 -0800

    added icons in Footer component

--- src/Components/footer.js
+++ src/Components/footer.js
@@ -1,5 +1,8 @@
 import React from "react";
 import Styled from "styled-components";
+import LinkedIn48 from "../Images/linkedin-48.png";
+import Github48 from "../Images/github-48.png";
+import Email48 from "../Images/email-48.png";
 
 const Footer = Styled.div`
     margin-top: 48px;
@@ -10,7 +13,6 @@ const Footer = Styled.div`
         display: grid;
         grid-template-columns: repeat(3, 33.33%);
         margin: auto;
-        border: 2px solid black;
         width: 400px;
         padding-left: 0;
         list-style: none;
@@ -26,9 +28,9 @@ const footer = () => {
     return (
         <Footer>
             <ul className="contact">
-                <li><a href="https://www.linkedin.com/in/adambwong/">LinkedIn</a></li>
-                <li><a href="https://github.com/awong017">GitHub</a></li>
-                <li><a href="mailto:adamwthedev@gmail.com">Email</a></li>
+                <li><a href="https://www.linkedin.com/in/adambwong/"><img src={LinkedIn48} alt="linked-in" /></a></li>
+                <li><a href="https://github.com/awong017"><img src={Github48} alt="github" /></a></li>
+                <li><a href="mailto:adamwthedev@gmail.com"><img src={Email48} alt="email" /></a></li>
             </ul>
             <p>2020, Adam Wong. Built with React</p>
         </Footer>
